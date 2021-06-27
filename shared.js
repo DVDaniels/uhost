4fbbc8692d8266f6482bb84e85f217e4386a20e3
David Daniels <David@DavidDaniels.com>
2021-06-27 14:27:50 -0400

    added aninmations to all pages

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta')
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
@@ -12,7 +13,10 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     // backdrop.style.display = "block";
     // modal.className = 'open'; this will actually override the complete class list
     modal.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(function() {
+        backdrop.classList.add('open');
+    }, 10);
 });
 }
 
@@ -33,12 +37,31 @@ function closeModal() {
     if (modal)  {
         modal.classList.remove('open');
     }
+    
     backdrop.classList.remove('open');
+    setTimeout(function() {
+        backdrop.style.display = 'none';
+    }, 200);
 }
 
 toggleButton.addEventListener('click', function() {
     // mobileNav.style.display = 'block';
     // backdrop.style.display = 'block';
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(function() {
+        backdrop.classList.add('open');
+    }, 10);
 });
+
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('Animation started', event)
+})
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('Animation ended', event)
+})
+
+ctaButton.addEventListener('animationiteratio ', function(event) {
+    console.log('Animation iteration', event)
+})
